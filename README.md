1b31992fb8b79266a5287ce545887e3722638f89
changwenyi <changwenyi@100tal.com>
2020-04-07 17:38:37 +0800

    feat: 🎸 init
    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vue-preset-mobile
-vue-preset-mobile
+Vue 项目模板移动端 preset
+
+### Usage
+```bash
+vue create --preset Tracy-Chang/vue-preset-mobile <project-name>
+```
+
+### Preset Config
+* babel, router, vuex, CSS Pre-processors, Linter / Formatter, Unit Testing
+* router mode: history
+* Sass/SCSS (with dart-sass)
+* eslint setup: ESLint + Standard config / Lint on save / Lint and fix on commit
+* test: Mocha + Chai
+* 配置文件放入新建文件中
+* [preset.json](./preset.json)
+
+### Features
+* 集成`git-cz`规范提交message（中文提示信息），强制验证提交信息
+* 集成`standard-version`自动生成changelog
+* 使用`postcss-px-to-viewport`插件，vw适配移动端
+* 预设三个 env 环境和三个 build scripts
+* 使用`compression-webpack-plugin`进行资源 gzip 打包
+* 使用`vconsole-webpack-plugin`集成 vconsole，生产环境默认关闭
+* Service 层封装 axios
+* 模块化 Vuex Store
+* 集成 editorconfig, gitattributes, jsconfig.json 等对代码优化的功能配置
---
+++ generator/index.js
@@ -0,0 +1,32 @@
+module.exports = (api, options, rootOptions) => {
+  // 复制并用 ejs 渲染 `./template` 内所有的文件
+  api.render('./template')
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    dependencies: {
+      'axios': '^0.18.0',
+      'js-cookie': '^2.2.0'
+    },
+    devDependencies: {
+      'git-cz': '^2.0.0',
+      'compression-webpack-plugin': '^2.0.0',
+      'vconsole-webpack-plugin': '^1.4.2',
+      'postcss-px-to-viewport': '^1.1.0'
+    },
+    scripts: {
+      'dev': 'vue-cli-service serve',
+      'build:testing': 'vue-cli-service build --mode testing',
+      'build:staging': 'vue-cli-service build --mode staging',
+      'build:production': 'vue-cli-service build',
+      'commit': 'git add . && npx git-cz'
+    },
+    config: {
+      commitizen: {
+        path: './node_modules/git-cz'
+      }
+    },
+    gitHooks: {
+      'commit-msg': 'node scripts/verifyCommitMsg.js'
+    }
+  })
+}
